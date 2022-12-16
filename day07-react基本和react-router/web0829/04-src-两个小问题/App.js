cf1db8c9d00295176248972571ba185f11c22ace
oldluo-ray <luodianlei7@163.com>
2022-12-16 14:43:22 +0800

    ''

--- day07-react基本和react-router/web0829/04-src-两个小问题/App.js
+++ day07-react基本和react-router/web0829/04-src-两个小问题/App.js
@@ -4,37 +4,16 @@ import React from 'react'
 // 一个Route定义一条前端路由规则.所以有几个页面(就应该定义几个Route).当然未来我们写的复杂了之后,可以一个页面,要写多个Route
 
 // Link的作用: 修改浏览器地址的路径,但是不发送请求
-import { Routes, Route, Link, NavLink } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
-
-import './App.css'
 export default function App() {
   return (
     <div>
       App
       {/* to属性的值 也可以忽略/*/}
-      <NavLink
-        to="home"
-        className={({ isActive }) => {
-          // 当我们切换路径的时候,这个回调会被执行
-          // console.log(obj)
-          // 这个函数中return的结果会赋值给className
-          return isActive ? 'xxx' : ''
-        }}
-      >
-        首页
-      </NavLink>
-      <NavLink
-        to="login"
-        style={({ isActive }) => {
-          return isActive
-            ? { fontSize: 30, color: 'pink', background: 'none' }
-            : {}
-        }}
-      >
-        登录页
-      </NavLink>
+      <Link to="home">首页</Link>
+      <Link to="login">登录页</Link>
       <hr />
       {/* <Home></Home>
       <Login></Login> */}
